61aae4795d56f73f930d5cfe9faa057c1e7218d5
Matthias Bussonnier <bussonniermatthias@gmail.com>
2022-04-10 18:26:46 +0200

    initial extension

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
+import { requestAPI } from './handler';
+
+/**
+ * Initialization data for the papyri-lab extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'papyri-lab:plugin',
+  autoStart: true,
+  optional: [ISettingRegistry],
+  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+    console.log('JupyterLab extension papyri-lab is activated!');
+
+    if (settingRegistry) {
+      settingRegistry
+        .load(plugin.id)
+        .then(settings => {
+          console.log('papyri-lab settings loaded:', settings.composite);
+        })
+        .catch(reason => {
+          console.error('Failed to load settings for papyri-lab.', reason);
+        });
+    }
+
+    requestAPI<any>('get_example')
+      .then(data => {
+        console.log(data);
+      })
+      .catch(reason => {
+        console.error(
+          `The papyri_lab server extension appears to be missing.\n${reason}`
+        );
+      });
+  }
+};
+
+export default plugin;
