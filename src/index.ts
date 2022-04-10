543c8e0295967270d711441f7d6fbc0c650eeab7
Matthias Bussonnier <bussonniermatthias@gmail.com>
2022-04-10 19:15:05 +0200

    works CP

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
 
 import { requestAPI } from './handler';
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+
+import { Widget } from '@lumino/widgets';
 
 /**
  * Initialization data for the papyri-lab extension.
@@ -13,9 +17,11 @@ import { requestAPI } from './handler';
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'papyri-lab:plugin',
   autoStart: true,
-  optional: [ISettingRegistry],
-  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+  optional: [ICommandPalette, ISettingRegistry],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette, settingRegistry: ISettingRegistry) => {
     console.log('JupyterLab extension papyri-lab is activated!');
+    console.log('ICommandPalette PPLab:', palette);
+    console.log('SettingRegistry PPLab:', settingRegistry);
 
     if (settingRegistry) {
       settingRegistry
@@ -27,6 +33,28 @@ const plugin: JupyterFrontEndPlugin<void> = {
           console.error('Failed to load settings for papyri-lab.', reason);
         });
     }
+
+    const content = new Widget();
+    const widget = new MainAreaWidget({ content });
+    widget.id = 'papyri-browser';
+    widget.title.label = 'Papyri browser';
+    widget.title.closable = true;
+
+    const command: string = 'papyri:open';
+    app.commands.addCommand(command, {
+      label: 'Open papyri browser',
+      execute: () => {
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.add(widget, 'main');
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+    // Add the command to the palette.
+
+    palette.addItem({ command, category: 'Papyri' });
 
     requestAPI<any>('get_example')
       .then(data => {
