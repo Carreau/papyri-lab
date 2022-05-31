0bb0b23c93b9428fb9915e8f4bde2463d8680b62
pdmurray <peynmurray@gmail.com>
2022-05-31 16:21:50 -0700

    Clean out code from attempts to use @jupyterlab/inspector

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,17 @@
 import {
-  ILabShell,
   ILayoutRestorer,
   JupyterFrontEnd,
   JupyterFrontEndPlugin,
 } from '@jupyterlab/application';
-import { PapyriWidget } from './widget';
+import { PapyriPanel } from './widget';
 
-import {
-  IInspector,
-  KernelConnector
-} from '@jupyterlab/inspector'
-
-import { INotebookTracker } from '@jupyterlab/notebook';
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
-
-import { PapyriInspectionHandler } from './handler'
 
 import {
   ICommandPalette,
   MainAreaWidget,
   WidgetTracker,
 } from '@jupyterlab/apputils';
-
-import { Token } from '@lumino/coreutils';
-
-
-export interface IPapyriInspector extends IInspector {}
-/**
- * The papyri inspector window token.
- */
-export const IPapyriInspector = new Token<IPapyriInspector>(
-  'papyri-lab/inspector:IPapyriInspector'
-);
 
 
 namespace CommandIDs {
@@ -47,7 +27,6 @@ const papyri: JupyterFrontEndPlugin<void> = {
   id: 'papyri-lab:plugin',
   autoStart: true,
   optional: [ICommandPalette, ISettingRegistry, ILayoutRestorer],
-  provides: IPapyriInspector,
   activate: async (
     app: JupyterFrontEnd,
     palette: ICommandPalette | null,
@@ -64,7 +43,7 @@ const papyri: JupyterFrontEndPlugin<void> = {
     }
 
     // Track and restore the widget state
-    const tracker = new WidgetTracker<MainAreaWidget<PapyriWidget>>({
+    const tracker = new WidgetTracker<MainAreaWidget<PapyriPanel>>({
       namespace: 'papyri',
     });
 
@@ -72,22 +51,18 @@ const papyri: JupyterFrontEndPlugin<void> = {
       return widget && !widget.isDisposed;
     }
 
-    let source: IInspector.IInspectable | null = null;
-    let widget: MainAreaWidget<PapyriWidget>;
+    let widget: MainAreaWidget<PapyriPanel>;
     const datasetKey = 'papyriInspector'
 
-    function openPapyri(args: string): MainAreaWidget<PapyriWidget> {
+    function openPapyri(): MainAreaWidget<PapyriPanel> {
       if (!isPapyriOpen()) {
         widget = new MainAreaWidget({
-          content: new PapyriWidget()
+          content: new PapyriPanel()
         })
         widget.id = 'papyri-browser'
         widget.title.label = 'Papyri browser'
         widget.title.closable = true
         void tracker.add(widget)
-        source = source && !source.isDisposed ? source : null
-        widget.content.source = source
-        widget.content.source?.onEditorChange(args)
       }
       if (!widget.isAttached) {
         app.shell.add(widget, 'main', {
@@ -110,12 +85,9 @@ const papyri: JupyterFrontEndPlugin<void> = {
       label: 'Open papyri browser',
       isEnabled: () => !widget || widget.isDisposed || !widget.isAttached || !widget.isVisible,
       execute: args => {
-        const text = args && (args.text as string);
         const refresh = args && (args.refresh as boolean);
-        if (isPapyriOpen() && refresh) {
-          widget.content.source?.onEditorChange(text)
-        } else {
-          openPapyri(text)
+        if (!isPapyriOpen() || refresh) {
+          openPapyri()
         }
       },
     });
@@ -129,23 +101,22 @@ const papyri: JupyterFrontEndPlugin<void> = {
 
     // Add papyri:toggle to the command registry
     app.commands.addCommand(CommandIDs.toggle, {
+      label: 'Toggle papyri browser',
       isToggled: () => isPapyriOpen(),
-      execute: args => {
+      execute: () => {
         if (isPapyriOpen()) {
           closePapyri()
         } else {
-          const text = args && (args.text as string);
-          openPapyri(text)
+          openPapyri()
         }
       }
     })
 
-    // Add the open command to the palette, if the palette is available.
+    // Add the commands above to the command palette, if available.
     if (palette) {
-      palette.addItem({ command: CommandIDs.toggle, category: 'Papyri' });
+      Object.values(CommandIDs).forEach(command => palette.addItem({ command, category: 'Papyri' }))
     }
 
-    // Add the c
     if (restorer) {
       void restorer.restore(tracker, {
         command: CommandIDs.toggle,
@@ -155,59 +126,6 @@ const papyri: JupyterFrontEndPlugin<void> = {
   },
 };
 
-const notebooks: JupyterFrontEndPlugin<void> = {
-  id: 'papyri-lab:notebooks',
-  autoStart: true,
-  requires: [IInspector, INotebookTracker, ILabShell],
-  activate: (
-    app: JupyterFrontEnd,
-    manager: IInspector,
-    notebooks: INotebookTracker,
-    labShell: ILabShell,
-  ): void => {
-    // Each notebook has a corresponding handler; these are kept here.
-    const handlers: { [id: string]: PapyriInspectionHandler } = {}
-
-    // Each time a notebook is created, a new handler is assigned to it.
-    notebooks.widgetAdded.connect((sender: any, parent: any) => {
-      const sessionContext = parent.sessionContext;
-      const rendermime = parent.content.rendermime;
-      const connector = new KernelConnector({ sessionContext });
-      const handler = new PapyriInspectionHandler({ connector, rendermime });
-
-      // Associate the handler to the widget.
-      handlers[parent.id] = handler;
-
-      // Set the initial editor.
-      const cell = parent.content.activeCell;
-      handler.editor = cell && cell.editor;
-
-      // Listen for active cell changes.
-      parent.content.activeCellChanged.connect((sender: any, cell: any) => {
-        handler.editor = cell && cell.editor;
-      });
-
-      // Listen for parent disposal.
-      parent.disposed.connect(() => {
-        delete handlers[parent.id];
-        handler.dispose();
-      });
-    });
-
-    // Keep track of notebook instances and set inspector source.
-    labShell.currentChanged.connect((sender, args) => {
-      const widget = args.newValue;
-      if (!widget || !notebooks.has(widget)) {
-        return;
-      }
-      const source = handlers[widget.id];
-      if (source) {
-        manager.source = source;
-      }
-    });
-  }
-}
-
-const plugins = [papyri, notebooks]
+const plugins = [papyri]
 
 export default plugins;
--- src/widget.tsx
+++ src/widget.tsx
@@ -6,7 +6,6 @@ import { requestAPI } from './handler';
 import { ServerConnection } from '@jupyterlab/services';
 import { URLExt } from '@jupyterlab/coreutils';
 import { Provider, Node } from '@nteract/mathjax';
-import { IInspector } from '@jupyterlab/inspector'
 
 import PapyriToolbar from './PapyriToolbar';
 import { ILocation, IBookmark } from './location';
@@ -36,12 +35,7 @@ const AdmStyle = style({
 /**
  * @returns Main Papyri component; contains documentation and navigation widgets
  */
-export function PapyriComponent({
-  source
-}: {
-  source: IInspector.IInspectable | null
-}): JSX.Element {
-  console.log({source})
+export function PapyriComponent(): JSX.Element {
   const [data, setData] = useState<Array<string>>([]);
   const [bookmarks, setBookmarks] = useState<Array<IBookmark>>([
     {
@@ -216,50 +210,16 @@ export function PapyriComponent({
 /**
  * A Lumino Widget that wraps a react PapyriWidget.
  */
-export class PapyriWidget extends ReactWidget {
+export class PapyriPanel extends ReactWidget {
   data: any;
   constructor() {
     super();
     this.addClass('jp-ReactWidget');
   }
 
-  get source(): IInspector.IInspectable | null {
-    return this._source;
-  }
-  set source(source: IInspector.IInspectable | null) {
-    if (this._source === source) {
-      return;
-    }
-
-    // Disconnect old signal handler.
-    if (this._source) {
-      this._source.standby = true;
-      this._source.inspected.disconnect(this.onInspectorUpdate, this);
-      this._source.disposed.disconnect(this.onSourceDisposed, this);
-    }
-
-    // Reject a source that is already disposed.
-    if (source && source.isDisposed) {
-      source = null;
-    }
-
-    // Update source.
-    this._source = source;
-
-    // Connect new signal handler.
-    if (this._source) {
-      this._source.standby = false;
-      this._source.inspected.connect(this.onInspectorUpdate, this);
-      this._source.disposed.connect(this.onSourceDisposed, this);
-    }
-  }
-
-
   render(): JSX.Element {
-    return <PapyriComponent source={this._source}/>;
+    return <PapyriComponent />;
   }
-
-  private _source: IInspector.IInspectable | null = null
 }
 
 const DSection = (props: any) => {
