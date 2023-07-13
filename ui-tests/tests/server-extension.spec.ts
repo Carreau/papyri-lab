a87a768f51aa4f8b549f18dc98874f90d635ea02
Amit Kumar <dtu.amit@gmail.com>
2023-07-13 13:45:06 +0100

    add a basic test

--- ui-tests/tests/server-extension.spec.ts
+++ ui-tests/tests/server-extension.spec.ts
@@ -1,40 +1,14 @@
-import { test, expect } from '@jupyterlab/galata';
+import { expect, test } from '@jupyterlab/galata';
 
-test.use({ autoGoto: false });
-
-test('should store state between reloads', async ({ page }) => {
-  await Promise.all([
-    page.waitForRequest(
-      request =>
-        request.url().search('jupyterlab-examples-server/hello') >= 0 &&
-        request.method() === 'GET'
-    ),
-    page.waitForRequest(
-      request =>
-        request.url().search('/jupyterlab-examples-server/hello') >= 0 &&
-        request.method() === 'POST' &&
-        request.postDataJSON()?.name === 'George'
-    ),
-    page.goto()
-  ]);
-
-  await page.waitForSelector('div[role="main"] >> text=Launcher');
-
-  await page
-    .waitForSelector('text=Get Server Content in a IFrame Widget')
-    .then(h => h.scrollIntoViewIfNeeded());
-
-  // Click the launcher widget to open an IFrame Widget
-  await page.click('text=Get Server Content in a IFrame Widget');
-
-  // Wait for div[role="main"] >> text=Server Doc
-  await page.waitForSelector('div[role="main"] >> text=Server Doc');
-
-  expect(
-    await page
-      .frame({ url: '/jupyterlab-examples-server/public/index.html' })
-      ?.waitForSelector(
-        'text=This content is served from the jupyterlab_examples_server extension.'
-      )
-  ).toBeTruthy();
+test.describe('Notebook Tests', () => {
+  test('Create New Notebook', async ({ page, tmpPath }) => {
+    const fileName = 'create_test.ipynb';
+    await page.notebook.createNew(fileName);
+    expect(
+      await page.waitForSelector(`[role="main"] >> text=${fileName}`)
+    ).toBeTruthy();
+    expect(await page.contents.fileExists(`${tmpPath}/${fileName}`)).toEqual(
+      true
+    );
+  });
 });
